f27172a084ce73af35f8034ec47e532bb2230ac7
Jorge Stiven Bernal Landazabal <jorgestivenbernallandazabal@Jorges-MacBook-Pro.local>
2022-02-19 14:20:24 -0500

    first commit

---
+++ src/main/java/com/stiven1946/automaticcash/AutomaticCashApplication.java
@@ -0,0 +1,13 @@
+package com.stiven1946.automaticcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticCashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stiven1946/automaticcash/AutomaticCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stiven1946.automaticcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticCashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
